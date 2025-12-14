3bbf0f500cb5f0e57f8b5a83b3809fab617e150d
sch61ul-student <sch61ul@gmail.com>
2025-12-14 14:56:10 +0400

    20 Update task.py

--- task.py
+++ task.py
@@ -1,15 +1,20 @@
-a = int(input())
-z = input()
-b = int(input())
-if z == "+":
-    print(a +  b)
-elif z == "-":
-    print(a - b)
-elif z == "*":
-    print(a * b)
-elif z == "/":
-    print(a / b)
-elif z == "^":
-    print(a ** b)
-else:
-    print( "Неизвестная операция" )
+class BankAccount:
+  
+    def __init__(self, number, sum):
+        self.account_number = number
+        self.balance = sum
+        self.status()
+        
+    def status(self):
+        print(f"Баланс счёта: {self.balance} единиц")
+     
+    def add(self, sum):
+        self.balance = self.balance + sum
+        print(f"На счет зачислено: {sum} единиц")
+         
+    def withdraw(self, sum):
+        if self.balance >= sum: 
+            self.balance = self.balance - sum
+            print(f"Со счета снято: {sum} единиц")
+        else:
+            print("Недостаточно средств на счете")
